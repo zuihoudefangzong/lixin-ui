5f918ee71acd4548ad380cc92b841a799ed32492
zuihoudefangzong <58388720@qq.com>
2022-04-19 10:38:20 +0800

    添加默认样式position-top

--- src/components/popover/popover.vue
+++ src/components/popover/popover.vue
@@ -78,8 +78,10 @@ export default {
       // div.popover的onclick事件的冒泡和document监听onclick导致刚点就又隐藏
       // 假如监听到触发的dom是this.$refs.popver或是this.$refs.popver后代节点Node的dom
       if(popover && (popover === e.target || popover.contains(e.target) )) { return }
+
+      // 因为最终contentWrapper最终会appchildren到body后面
       // 假如监听到触发的dom是this.$refs.contentWrapper或是this.$refs.contentWrapper后代节点Node的dom
-      // if(contentWrapper && (contentWrapper === e.target || contentWrapper.contains(e.target) )) { return }
+      if(contentWrapper && (contentWrapper === e.target || contentWrapper.contains(e.target) )) { return }
       // 点到document文档其他dom就隐藏this.$refs.triggerWrapper
       console.log('document隐藏的')
       this.close()
@@ -104,6 +106,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border-radius: 4px;
 .popover {
   display: inline-block;
   // 对齐方式
@@ -115,21 +118,45 @@ export default {
     position: absolute;
     color: #606266;
     border: 1px solid #ebeef5;
+    // 两个阴影都可以
+    // filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
     box-shadow: 0 2px 12px 0 rgba(0,0,0,0.1);
+    border-radius: $border-radius;
     padding: 18px 20px;
     background: #fff;
-    // 小三角
+    // 文字过多限制
+    max-width: 20em;
+    // 自动换行
+    word-break: break-all;
+    // 下面的小三角
     &::before, &::after{
+      content: '';
       position: absolute;
       display: block;
       width: 0;
       height: 0;
-      border-color: transparent;
       border: 10px solid transparent;
     }
+    // 默认上方top
     &.position-top {
       transform: translateY(-100%);
       margin-top: -10px;
+      // 覆盖默认的::before和
+      &::before {
+        border-top-color: #ebeef5;
+        border-bottom: none;
+        top: 100%;
+      }
+      // 覆盖默认的::after
+      &::after {
+        border-top-color: white;
+        border-bottom: none;
+        top: calc(100% - 2px);
+      }
+      // 在往左边距离10px就够了
+      &::before,&::after {
+        left: 10px;
+      }
     }
 }
 </style>
